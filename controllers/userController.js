cd9abd37e1e24031d721330ee7c91cb4dad1f427
Anuththara Sathyajith <wickramage28@gmail.com>
2025-03-16 13:38:46 +0530

    git init👍💪✔💥

---
+++ controllers/userController.js
@@ -0,0 +1,72 @@
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+import JsonWebToken from "jsonwebtoken";
+import dotenv from "dotenv";
+
+export function registerUser (req,res){
+
+    const data = req.body;
+    data.password = bcrypt.hashSync(data.password,10)
+    
+    const newUser = new User(data)
+
+    newUser.save().then(()=>{
+        res.json({message : "User Registered successfully"})
+    }).catch((error)=>{
+        console.log(error)
+        res.status(500).json({message : "User registered failed"})
+    })
+}
+
+export function loginUser(req,res){
+    const data = req.body;
+    
+
+    User.findOne({
+        email : data.email
+    }).then((user)=>{
+
+        if(user== null) {
+            res.status(404).json({error: "User not found"})
+        }else{
+            const isPasswordCorrect = bcrypt.compareSync
+            (data.password,user.password);
+
+            if(isPasswordCorrect){
+                const token = JsonWebToken.sign({
+                    firstName : user.firstName,
+                    lastName  : user.lastName,
+                    email : user.email,
+                    role : user.role,
+                    profilePicture :user.profilePicture,
+                    phone : user.phone,
+                },process.env.JWT_SECRET)
+                res.json({message : "Login successful",token : token, user:user});
+
+
+            }else{
+                res.status(404).json({error: "Login failed"});
+            }
+        }
+    })
+
+}
+
+
+export function isItAdmin(req){
+
+    let isAdmin=false;
+    if(req.user != null && req.user.role == "admin"){
+        isAdmin = true;
+    }
+    return isAdmin;
+}
+
+export function isItCustomer(req){
+
+    let isCustomer=false;
+    if(req.user != null && req.user.role == "customer"){
+        isCustomer = true;
+    }
+    return isCustomer;
+}
---
+++ index.js
@@ -0,0 +1,66 @@
+import bodyParser from "body-parser";
+import express from "express"
+import mongoose from "mongoose";  
+import userRouter from "./routes/userRouter.js";
+import productRouter from "./routes/productRouter.js";
+import jwt from "jsonwebtoken";
+import reviewRouter from "./routes/reviewRouter.js";
+import dotenv from "dotenv"
+import inquiryRouter from "./routes/inquiryRouter.js";
+import cors from "cors";
+
+dotenv.config()
+//.env file eka thibune routes folder eka athule ekai aula. eka root folder ekata danna ona
+//e kiyanne project folder eke mulatama dana ona
+
+const app = express()
+app.use(cors());
+
+app.use(bodyParser.json()); //middleware parser library 
+
+app.use((req,res,next)=>{   //create middleware
+    
+    let token= req.header
+    ("Authorization");
+    
+    if(token!=null){
+        token = token.replace("Bearer ","");
+
+        jwt.verify(token,process.env.JWT_SECRET,
+        (err, decoded) => {
+            //console.log(err);
+            if(!err){
+                req.user = decoded;
+                
+            }
+        });
+    }
+    next()
+
+    
+});
+
+
+let mongoUrl = process.env.MONGO_URL
+mongoose.connect(mongoUrl)               //methanin thamai kiyanne mongo url ekath ekka connect wenna kiyala
+
+const connection = mongoose.connection   //me mongoose kiyana library eke thiyena connection eka araganna kiyanawa
+connection.once("open",()=>{
+    console.log("MongoDB Connection established sucessfully")
+})
+
+app.use("/api/users",userRouter);
+app.use("/api/products",productRouter);
+app.use("/api/reviews",reviewRouter);
+app.use("/api/inquiries",inquiryRouter);
+
+
+
+//testuser@example.com -customer
+//testadmin@example.com-admin
+
+
+app.listen(3002,()=>{
+    console.log("Server is running on port 3002")
+});
+
